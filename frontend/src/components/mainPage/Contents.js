d715ce27f4157f2f86a87d83100ac43e0f3b22a5
wangkunyi <kunyiwang252750@gmail.com>
2023-06-08 21:59:15 -0700

    small update

--- frontend/src/components/mainPage/Contents.js
+++ frontend/src/components/mainPage/Contents.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Contents.css';
 import { useState, useEffect } from 'react';
 import PreviewCard from "../PreviewCard";
+import { useNavigate } from "react-router-dom";
 
 const Contents = () => {
 
     const [posts, setPosts] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleClick = (index) => {
+        navigate(`/post/${index+1}`);
+    };
 
     useEffect(() => {
         fetch(`http://localhost:8080/posts`)
@@ -27,7 +34,10 @@ const Contents = () => {
             <ul className="flex flex-row flex-wrap justify-center">
                 {posts.map((post, index) => (   
                     <li key={index}>
-                        <PreviewCard src={post.mediaUrl} title={post.title} previewText={post.text}/>
+                        <PreviewCard src={post.mediaUrl} title={post.title}
+                                     previewText={post.text} className="child-item"
+                                     onClick={() => handleClick(index)}
+                        />
                     </li>
                 ))}
             </ul>
