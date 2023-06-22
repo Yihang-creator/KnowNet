3817cf37977675c8a88f83b4cfea18abf2c6bc20
Hanze Wang <hanzewang@dhcp-128-189-235-108.ubcsecure.wireless.ubc.ca>
2023-06-22 14:34:13 -0700

    css

--- frontend/src/components/comments/Comment.jsx
+++ frontend/src/components/comments/Comment.jsx
@@ -3,11 +3,15 @@ import '../../Styles/Comment.css';
 
 const Comment = ({ comment, addReply, depth }) => {
     const [newReply, setNewReply] = useState('');
+    const [showReplyInput, setShowReplyInput] = useState(false);
 
     const handleAddReply = () => {
         if (newReply.trim() !== '') {
             addReply(comment.id, newReply);
             setNewReply('');
+            setShowReplyInput(false);
+        } else {
+            setShowReplyInput(!showReplyInput);
         }
     };
 
@@ -21,26 +25,41 @@ const Comment = ({ comment, addReply, depth }) => {
         <div style={commentStyle} className="comment">
             <div className="comment-content">
                 <p className="comment-text">{comment.text}</p>
-                <p className="comment-timestamp">{new Date(comment.timestamp).toLocaleString()}</p>
+                <p className="comment-timestamp">
+                    {new Date(comment.timestamp).toLocaleString()}
+                </p>
             </div>
             <div className="comment-replies">
-                {comment.replies && comment.replies.map((reply, index) => (
-                    <Comment
-                        key={reply.id}
-                        comment={reply}
-                        addReply={addReply}
-                        depth={depth + 1}
-                    />
-                ))}
+                {comment.replies &&
+                    comment.replies.map((reply, index) => (
+                        <Comment
+                            key={reply.id}
+                            comment={reply}
+                            addReply={addReply}
+                            depth={depth + 1}
+                        />
+                    ))}
             </div>
-            <div className="comment-input">
-                <textarea
-                    value={newReply}
-                    onChange={(e) => setNewReply(e.target.value)}
-                    placeholder="Type your reply here..."
-                />
-                <button onClick={handleAddReply}>Reply</button>
-            </div>
+            {showReplyInput && (
+                <div className="comment-input">
+          <textarea
+              value={newReply}
+              onChange={(e) => setNewReply(e.target.value)}
+              placeholder="Type your reply here..."
+          />
+                    <button className="reply-button" onClick={handleAddReply}>
+                        Reply
+                    </button>
+                </div>
+            )}
+            {!showReplyInput && (
+                <button
+                    className="reply-button"
+                    onClick={() => setShowReplyInput(true)}
+                >
+                    Reply
+                </button>
+            )}
         </div>
     );
 };
