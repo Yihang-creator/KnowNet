06c26bdaa9c6c950414ca2ab2222782b1c360d5c
yhe25 <yhe25@students.cs.ubc.ca>
2023-06-27 00:33:58 -0400

    fix deployment issue

--- server/app.js
+++ server/app.js
@@ -26,5 +26,5 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 app.use("/api/upload", awsRouter);
 
-app.listen(8080,() => console.log('Server is running on port 8080'));
+app.listen(process.env.PORT || 8080,() => console.log('Server is running on port 8080'));
 
