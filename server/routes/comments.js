a1adf5a2e7b1732b7bddddae2981e2926a3ba7b4
Hanze Wang <hanzewang@dhcp-128-189-236-164.ubcsecure.wireless.ubc.ca>
2023-06-29 12:58:03 -0700

    api for comment

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const {body, validationResult} = require("express-validator");
 var router = express.Router();
 
 const comments = [
@@ -38,13 +39,69 @@ const comments = [
   ];
 
 
+
+// get all comments
 router.get('/', function(req, res, next) {
     const postId = parseInt(req.query.postId);
 
+    if (isNaN(postId)) {
+        return res
+            .status(400)
+            .json({ error: 'Invalid postId.' });
+    }
+    const relatedComments = comments.filter(comment => comment.postId === postId);
     return res
-        .setHeader('Content-Type', 'application/json')
-        .send(comments.filter(comment => comment.postId === postId));
+        .status(200)
+        .json(relatedComments);
 });
+
+//delete a comment
+router.delete('/:id', function(req, res, next) {
+    const id = req.params.id;
+    const index = comments.findIndex(comment => comment.id === id);
+
+    if (index === -1) {
+        return res
+            .status(404)
+            .setHeader('Content-Type', 'application/json')
+            .send({ error: 'Comment not found' });
+    }
+
+    const deletedComment = comments.splice(index, 1);
+
+    return res
+        .status(200)
+        .setHeader('Content-Type', 'application/json')
+        .send(deletedComment);
+});
+
+
+// add a comment
+router.post('/', [
+    body('postId').notEmpty(),
+    body('userId').notEmpty(),
+    body('text').isLength({ min: 1 })
+], function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { postId, userId, text } = req.body;
+    const newComment = {
+        "id": uuidv4(),
+        "postId": postId,
+        "userId": userId,
+        "text": text,
+        "timestamp": new Date().toISOString(),
+        "replies": []
+    };
+
+    comments.push(newComment);
+
+    res.status(201).json(newComment);
+});
+
 
 
 module.exports = router;
