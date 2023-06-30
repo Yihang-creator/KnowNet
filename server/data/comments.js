5cc16bb0479d37f29cb433371a9400df7be6fccb
Yingquan Wang <yingquanwangubc@gmail.com>
2023-06-29 21:39:57 -0700

    newdata

---
+++ server/data/comments.js
@@ -0,0 +1,28 @@
+let comments = [
+    {
+      "commentId": 1,
+      "postId": 1,
+      "userId": 2,
+      "text": "I love this blog!",
+      "timestamp": "2023-06-08T00:00:00Z",
+      "replies": [1]
+    },
+    {
+      "commentId": 2,
+      "postId": 1,
+      "userId": 1,
+      "text": "I have a secret to tell you",
+      "timestamp": "2023-06-08T00:05:00Z",
+      "replies": []
+    },
+    {
+      "commentId": 3,
+      "postId": 2,
+      "userId": 1,
+      "text": "Nice post!",
+      "timestamp": "2023-06-08T00:10:00Z",
+      "replies": []
+    }
+  ]
+
+  module.exports.comments = comments;
---
+++ server/data/replies.js
@@ -0,0 +1,10 @@
+let replies = [{
+  "replyId": 1,
+  "repalyToId": 2,
+  "repalyToName": 2,
+  "userId": 2,
+  "text": "What is it?",
+  "timestamp": "2023-06-08T00:06:00Z",
+}]
+
+module.exports.replies = replies;
