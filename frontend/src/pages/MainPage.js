616da86406141d7f86ce865f7547be2eee6b0680
Hanze Wang <hanzewang@dhcp-128-189-235-81.ubcsecure.wireless.ubc.ca>
2023-07-15 02:12:10 -0700

    edited search bar, drop down list, comment board

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -3,50 +3,50 @@ import React, { useState } from "react";
 import Contents from "../components/mainPage/Contents";
 import { Divider } from "@mui/material";
 import SearchBar from "../components/mainPage/SearchBar";
-import Dropdown from "../components/mainPage/Dropdown";
+// import Dropdown from "../components/mainPage/Dropdown";
 import CreatePostButton from "../components/mainPage/CreatePostButton";
 import JoinVideoRoomButton from "../components/mainPage/JoinVideoRoomButton";
 import { useUserContext } from "../auth/UserContext";
 
 const Home = () => {
-  const { authState } = useOktaAuth();
-  // const [userInfo, setUserInfo] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("");
-  const { userInfo } = useUserContext();
+    const { authState } = useOktaAuth();
+    // const [userInfo, setUserInfo] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
+    const { userInfo } = useUserContext();
 
-  console.log(userInfo);
+    console.log(userInfo);
 
-  // const login = useCallback(async () => {
-  //   await oktaAuth.signInWithRedirect();
-  // }, [oktaAuth]);
-  // useEffect(() => {
-  //   if (!authState || !authState.isAuthenticated) {
-  //     // When user isn't authenticated, forget any user info
-  //     setUserInfo(null);
-  //     // If user is not authenticated, redirect to Okta login
-  //     login();
-  //   } else {
-  //     oktaAuth.getUser().then((info) => {
-  //       setUserInfo(info);
-  //     });
-  //   }
-  // }, [authState, oktaAuth, login]);
+    // const login = useCallback(async () => {
+    //   await oktaAuth.signInWithRedirect();
+    // }, [oktaAuth]);
+    // useEffect(() => {
+    //   if (!authState || !authState.isAuthenticated) {
+    //     // When user isn't authenticated, forget any user info
+    //     setUserInfo(null);
+    //     // If user is not authenticated, redirect to Okta login
+    //     login();
+    //   } else {
+    //     oktaAuth.getUser().then((info) => {
+    //       setUserInfo(info);
+    //     });
+    //   }
+    // }, [authState, oktaAuth, login]);
 
-  if (!authState) {
-    return <div>Loading...</div>;
-  }
+    if (!authState) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <Dropdown />
-      <div className="flex">
-        <CreatePostButton />
-        <Divider variant="middle" />
-        <JoinVideoRoomButton />
-      </div>
-      <Contents searchTerm={searchTerm} />
-    </div>
-  );
+    return (
+        <div>
+            <SearchBar setSearchTerm={setSearchTerm} />
+            {/* <Dropdown /> */}
+            <div className="flex">
+                <CreatePostButton />
+                <Divider variant="middle" />
+                <JoinVideoRoomButton />
+            </div>
+            <Contents searchTerm={searchTerm} />
+        </div>
+    );
 };
 export default Home;
