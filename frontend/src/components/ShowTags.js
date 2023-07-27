ecd8058c6a543be43872e5018942024eb3549994
Yingquan Wang <yingquanwangubc@gmail.com>
2023-07-27 00:32:11 -0700

    #beforeTags

--- frontend/src/components/ShowTags.js
+++ frontend/src/components/ShowTags.js
@@ -3,10 +3,10 @@ import { Button } from "@mui/material";
 
 const ShowTags = (props) => {
   return (<>
-    <h2 style={{ display: 'inline'}}>Tags: </h2>
+    {/* <h2 style={{ display: 'inline'}}>Tags: </h2> */}
     <Stack style={{ display: 'inline'}} direction="row" spacing={2}>
       {props.tags.map((tag) => (
-        <Button variant="text">{tag}</Button>
+        <Button variant="text">#{tag}</Button>
       ))}
     </Stack>
     </>
