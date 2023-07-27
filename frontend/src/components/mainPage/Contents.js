b399aa26f40b2cc80f6b7077f22ee6157f3d71d4
Yingquan Wang <yingquanwangubc@gmail.com>
2023-07-27 14:32:10 -0700

    searchtag

--- frontend/src/components/mainPage/Contents.js
+++ frontend/src/components/mainPage/Contents.js
@@ -5,35 +5,55 @@ import { Link } from "react-router-dom";
 import { useOktaAuth } from "@okta/okta-react";
 import { fetchAllPost } from "../../redux/actions/PostActions";
 import { useSelector, useDispatch } from "react-redux";
-import Loading from '../../auth/Loading';
-import CardMedia from '@mui/material/CardMedia';
-import Carousel from 'react-material-ui-carousel'
+import Loading from "../../auth/Loading";
+import CardMedia from "@mui/material/CardMedia";
+import Carousel from "react-material-ui-carousel";
 
-const Contents = ({searchTerm}) => {
+const Contents = ({ searchTerm, searchByTag}) => {
   const { oktaAuth } = useOktaAuth();
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
 
   useEffect(() => {
-    dispatch(fetchAllPost(oktaAuth.getAccessToken()))
+    dispatch(fetchAllPost(oktaAuth.getAccessToken()));
   }, [dispatch, oktaAuth]);
 
   if (!posts) {
-    return (<Loading />);
+    return <Loading />;
   }
 
-  const filteredPosts = searchTerm
-      ? posts.filter((post) =>
-          post.title.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      : posts;
+  const filteredByTag = function (posts, searchTerm) {
+    const filtered = [];
+    for (const post of posts) {
+      if (post.tags.map(string => string.toLowerCase()).includes(searchTerm.toLowerCase())) {
+        filtered.push(post)
+      }
+    }
+    return filtered;
+  }
+
+  const getFilteredPost = function (searchTerm, searchByTag) {
+    // const searchByTag = false;
+    if (!searchTerm) return posts;
+    if (!searchByTag) {
+      return posts.filter((post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+    return filteredByTag(posts, searchTerm);
+  };
+
+  
+
+  const filteredPosts = getFilteredPost(searchTerm, searchByTag);
+
 
   // const firstFivePosts = filteredPosts.filter(post => post.mediaType === 'image').slice(0, 5);
 
   return (
-      <div className="flex justify-center mt-10">
-        <div className="flex-container justify-center rounded-lg border bg-grey-600 w-11/12">
-          <ul className="rounded-md p-2 md:columns-2 lg:columns-4">
+    <div className="mt-10 flex justify-center">
+      <div className="flex-container bg-grey-600 w-11/12 justify-center rounded-lg border">
+        <ul className="rounded-md p-2 md:columns-2 lg:columns-4">
           {/* <li className="max-h-[500px] max-w-[500px] overflow-hidden">
               <Carousel navButtonsAlwaysVisible={true}>
                     {firstFivePosts.map((post, index) => (
@@ -48,23 +68,26 @@ const Contents = ({searchTerm}) => {
                     ))}
               </Carousel>
             </li> */}
-            {filteredPosts.map((post, index) => (
-                <li key={index}>
-                  <Link to={`/post/${post.postId}`} className="inline-block w-full p-2">
-                    <PreviewCard
-                        type={post.mediaType}
-                        src={post.mediaUrl}
-                        title={post.title}
-                        previewText={post.text}
-                        username={post.username}
-                        userPhotoUrl={post.userPhotoUrl}
-                    />
-                  </Link>
-                </li>
-            ))}
-          </ul>
-        </div>
+          {filteredPosts.map((post, index) => (
+            <li key={index}>
+              <Link
+                to={`/post/${post.postId}`}
+                className="inline-block w-full p-2"
+              >
+                <PreviewCard
+                  type={post.mediaType}
+                  src={post.mediaUrl}
+                  title={post.title}
+                  previewText={post.text}
+                  username={post.username}
+                  userPhotoUrl={post.userPhotoUrl}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
+    </div>
   );
 };
 
