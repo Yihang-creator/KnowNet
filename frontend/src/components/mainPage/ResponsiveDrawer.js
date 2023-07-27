e5a8c400a24b665f1cccc5212ae2c86251f2c750
Hanze Wang <hanzewang@dhcp-128-189-238-229.ubcsecure.wireless.ubc.ca>
2023-07-27 13:48:14 -0700

    add searchbar and toolbar

--- frontend/src/components/mainPage/ResponsiveDrawer.js
+++ frontend/src/components/mainPage/ResponsiveDrawer.js
@@ -13,9 +13,10 @@ import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import ChatIcon from '@mui/icons-material/Chat';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import CreatePostButton from './CreatePostButton';
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const drawerWidth = 240;
 
@@ -23,69 +24,80 @@ function ResponsiveDrawer(props) {
     const theme = useTheme();
 
     const drawer = (
-      <div>
-        <Toolbar />
-        <Divider />
-        <List>
-          <Link to={`/`} >
-              <ListItem key={'Home'} disablePadding>
-              <ListItemButton>
-                  <ListItemIcon sx={{ color: theme.palette.primary.main }}>
-                      <HomeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={'Home'} sx={{ color: theme.palette.text.primary }}/>
-              </ListItemButton>
-              </ListItem>
-          </Link>
-          <CreatePostButton/>
-          <Link to={`/joinVideoRoom`} className="group">
-              <ListItem key={'Video Room'} disablePadding>
-              <ListItemButton>
-                  <ListItemIcon sx={{ color: theme.palette.primary.main }}>
-                      <YouTubeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={'Video Room'} sx={{ color: theme.palette.text.primary }}/>
-              </ListItemButton>
-              </ListItem>
-          </Link>
-          <ListItem key={'Chats'} disablePadding>
-          <ListItemButton>
-              <ListItemIcon sx={{ color: theme.palette.primary.main }}>
-                  <ChatIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Chats'} sx={{ color: theme.palette.text.primary }}/>
-          </ListItemButton>
-          </ListItem>
-        </List>
-      </div>
+        <div>
+            <Toolbar/>
+            <Divider/>
+            <List>
+                <Link to={`/`}>
+                    <ListItem key={'Home'} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon sx={{color: theme.palette.primary.main}}>
+                                <HomeIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary={'Home'} sx={{color: theme.palette.text.primary}}/>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+                <CreatePostButton/>
+                <Link to={`/joinVideoRoom`} className="group">
+                    <ListItem key={'Video Room'} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon sx={{color: theme.palette.primary.main}}>
+                                <YouTubeIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary={'Video Room'} sx={{color: theme.palette.text.primary}}/>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+                <ListItem key={'Chats'} disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon sx={{color: theme.palette.primary.main}}>
+                            <ChatIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary={'Chats'} sx={{color: theme.palette.text.primary}}/>
+                    </ListItemButton>
+                </ListItem>
+                <Link to={`/profile`} className="group">
+                    <ListItem key={'Profile'} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon sx={{color: theme.palette.primary.main}}>
+                                <AccountCircleIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'Profile'} sx={{color: theme.palette.text.primary}}/>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+
+            </List>
+        </div>
     );
 
-  //setting zIndex to 900 make left bar appear under topbar
-  return (
-    <Box sx={{ display: 'flex', zIndex: 900 }}> 
-      <CssBaseline />
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-      >
-        <Drawer
-          variant="permanent"
-          sx={{
-            zIndex: 900,
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-    </Box>
-  );
+    //setting zIndex to 900 make left bar appear under topbar
+    return (
+        <Box sx={{display: 'flex', zIndex: 900}}>
+            <CssBaseline/>
+            <Box
+                component="nav"
+                sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+            >
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        zIndex: 900,
+                        display: {xs: 'none', sm: 'block'},
+                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+        </Box>
+    );
 }
 
 ResponsiveDrawer.propTypes = {
-  window: PropTypes.func,
+    window: PropTypes.func,
 };
 
 export default ResponsiveDrawer;
--- frontend/src/components/userPages/UserInfoPage.js
+++ frontend/src/components/userPages/UserInfoPage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Box, Typography, CardContent, Grid, Chip, Fab, Zoom } from '@mui/material';
+import { Box, Typography, CardContent, Grid, Chip, Fab, Zoom, Toolbar} from '@mui/material';
 import { Face } from '@mui/icons-material'
 import { useTheme } from '@mui/material/styles';
 import PopupButton from "./PopupButton";
@@ -12,6 +12,8 @@ import DoneIcon from '@mui/icons-material/Done';
 import EditIcon from "@mui/icons-material/Edit";
 import PostEdit from "../mainPage/postEdit";
 import SearchBar from '../mainPage/SearchBar';
+import ResponsiveDrawer from '../mainPage/ResponsiveDrawer';
+
 const UserInfoPage = ({ name, email }) => {
   const { userInfo } = useUserContext();
   const theme = useTheme();
@@ -99,117 +101,128 @@ const UserInfoPage = ({ name, email }) => {
   }
 
   return (
-      <div className="flex flex-col gap-4 p-2">
-        <SearchBar setSearchTerm={setSearchTerm}/>
-        <Box
-            sx={{
-              background: 'linear-gradient(#041a2d, #515f72)',
-              borderRadius: '5px'
-            }}
-        >
-          {/*<div className="float-right mr-2 mt-2">*/}
-          {/*  <Dropdown />*/}
-          {/*</div>*/}
-          <Grid className="flex p-5 pt-7">
-            <div className="relative h-36 w-36 justify-center mt-4 rounded-full bg-white">
-            <div className="absolute bottom-0 right-4 z-10">
-                <label
-                    htmlFor="profileUpload"
-                    className="flex h-7 w-7 items-center justify-center rounded-full bg-yellow-500 font-bold text-black shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                >
-                  +
-                </label>
-                <input
-                    type="file"
-                    id="profileUpload"
-                    className="hidden"
-                    onChange={handleImageUpload}
-                />
-              </div>
-              <div className="absolute inset-0 overflow-hidden rounded-full">
-                {selectedImage && (
-                    <img
-                        className="h-full w-full"
-                        src={selectedImage}
-                        alt="profile"
-                    />
-                )}
-              </div>
-            </div>
-            <Box className="items-center flex pl-2">
-              <CardContent sx={{ flex: '1 0 auto' }}>
-                <Typography component="div" variant="h4" color="#fff" sx={{ marginBottom: '12px' }}>
-                  {name}
-                  {/*<Chip sx={{ color: '#bfbdbd', marginLeft: '20px' }} icon={<Face color="#bfbdbd" sx={{ fontSize: '20px', color: '#bfbdbd' }} />} label="23" />*/}
-                </Typography>
-                <Typography variant="subtitle1" component="div" color="#bfbdbd">
-                  @{email}
-                </Typography>
-              </CardContent>
-            </Box>
-          </Grid>
-          <Grid className="flex justify-between p-2 text-white">
-            <Grid className="flex gap-2">
-              <PopupButton type="followings" token={oktaAuth.getAccessToken()} />
-              <PopupButton type="followers" token={oktaAuth.getAccessToken()} />
-            </Grid>
-          </Grid>
+      <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex', overflow: 'auto'}}>
+          <ResponsiveDrawer />
+          <Box sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, maxWidth: '100%', overflowX: 'hidden' }}>
+            <Toolbar />
+          </Box>
         </Box>
-
-        <Grid sx={{ flexDirection: 'column' }} className="flex rounded-md border-2 bg-gray-400 p-2 text-white">
-          <Grid className="flex justify-center gap-4 rounded-md p-2">
-            <button className="rounded-md border-2 p-2">Posts</button>
-            <button className="rounded-md border-2 p-2">Liked</button>
-          </Grid>
-
-          <Grid className="rounded-md bg-white p-2 md:columns-2 lg:columns-4">
-            {posts.map((post, index) => (
-                <div key={index} className="inline-block w-full p-2 relative">
-                  <Link to={`/post/${post.postId}`}>
-                    <ProfileCard
-                        type={post.mediaType}
-                        src={post.mediaUrl}
-                        title={post.title}
-                        previewText={post.text}
-                    />
-                  </Link>
-                  {
-                    editStatus ? <Fab
-                        variant="extended"
-                        sx={{
-                          position: 'absolute',
-                          left: '20px',
-                          top: '20px',
-                          width: '20px',
-                          opacity: '0.5',
-                          zIndex: '10'
-                        }}
-                        aria-label="edit"
-                        onClick={() => handleClickOpen(post)}
-                    >
-                      <EditIcon />
-                    </Fab> : ''
-                  }
-                </div>
-            ))}
-            <Zoom
-                key={fab.color}
-                in={true}
-                timeout={transitionDuration}
-                style={{
-                  transitionDelay: `${transitionDuration.exit}ms`,
+        <Box sx={{ flexGrow: 1 }}>
+          <div className="flex flex-col gap-4 p-2">
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Box
+                sx={{
+                  background: 'linear-gradient(#041a2d, #515f72)',
+                  borderRadius: '5px'
                 }}
-                unmountOnExit
             >
-              <Fab sx={fab.sx} aria-label={fab.label} color={fab.color} onClick={handleEditStatus}>
-                {fab.icon}
-              </Fab>
-            </Zoom>
-            <PostEdit post={editPost} open={open} handleClose={handleClose}/>
-          </Grid>
-        </Grid>
-      </div>
+              {/*<div className="float-right mr-2 mt-2">*/}
+              {/*  <Dropdown />*/}
+              {/*</div>*/}
+              <Grid className="flex p-5 pt-7">
+                <div className="relative h-36 w-36 justify-center mt-4 rounded-full bg-white">
+                  <div className="absolute bottom-0 right-4 z-10">
+                    <label
+                        htmlFor="profileUpload"
+                        className="flex h-7 w-7 items-center justify-center rounded-full bg-yellow-500 font-bold text-black shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    >
+                      +
+                    </label>
+                    <input
+                        type="file"
+                        id="profileUpload"
+                        className="hidden"
+                        onChange={handleImageUpload}
+                    />
+                  </div>
+                  <div className="absolute inset-0 overflow-hidden rounded-full">
+                    {selectedImage && (
+                        <img
+                            className="h-full w-full"
+                            src={selectedImage}
+                            alt="profile"
+                        />
+                    )}
+                  </div>
+                </div>
+                <Box className="items-center flex pl-2">
+                  <CardContent sx={{ flex: '1 0 auto' }}>
+                    <Typography component="div" variant="h4" color="#fff" sx={{ marginBottom: '12px' }}>
+                      {name}
+                      {/*<Chip sx={{ color: '#bfbdbd', marginLeft: '20px' }} icon={<Face color="#bfbdbd" sx={{ fontSize: '20px', color: '#bfbdbd' }} />} label="23" />*/}
+                    </Typography>
+                    <Typography variant="subtitle1" component="div" color="#bfbdbd">
+                      @{email}
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Grid>
+              <Grid className="flex justify-between p-2 text-white">
+                <Grid className="flex gap-2">
+                  <PopupButton type="followings" token={oktaAuth.getAccessToken()} />
+                  <PopupButton type="followers" token={oktaAuth.getAccessToken()} />
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Grid sx={{ flexDirection: 'column' }} className="flex rounded-md border-2 bg-gray-400 p-2 text-white">
+              <Grid className="flex justify-center gap-4 rounded-md p-2">
+                <button className="rounded-md border-2 p-2">Posts</button>
+                <button className="rounded-md border-2 p-2">Liked</button>
+              </Grid>
+
+              <Grid className="rounded-md bg-white p-2 md:columns-2 lg:columns-4">
+                {posts.map((post, index) => (
+                    <div key={index} className="inline-block w-full p-2 relative">
+                      <Link to={`/post/${post.postId}`}>
+                        <ProfileCard
+                            type={post.mediaType}
+                            src={post.mediaUrl}
+                            title={post.title}
+                            previewText={post.text}
+                        />
+                      </Link>
+                      {
+                        editStatus ? <Fab
+                            variant="extended"
+                            sx={{
+                              position: 'absolute',
+                              left: '20px',
+                              top: '20px',
+                              width: '20px',
+                              opacity: '0.5',
+                              zIndex: '10'
+                            }}
+                            aria-label="edit"
+                            onClick={() => handleClickOpen(post)}
+                        >
+                          <EditIcon />
+                        </Fab> : ''
+                      }
+                    </div>
+                ))}
+                <Zoom
+                    key={fab.color}
+                    in={true}
+                    timeout={transitionDuration}
+                    style={{
+                      transitionDelay: `${transitionDuration.exit}ms`,
+                    }}
+                    unmountOnExit
+                >
+                  <Fab sx={fab.sx} aria-label={fab.label} color={fab.color} onClick={handleEditStatus}>
+                    {fab.icon}
+                  </Fab>
+                </Zoom>
+                <PostEdit post={editPost} open={open} handleClose={handleClose}/>
+              </Grid>
+            </Grid>
+          </div>
+        </Box>
+      </Box>
   );
+
 };
 
 export default UserInfoPage;
