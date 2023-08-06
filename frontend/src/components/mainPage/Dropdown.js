dd118207bd4ed7d7fc93c96858177586a7ec306e
Hanze Wang <hanzewang@dhcp-128-189-236-131.ubcsecure.wireless.ubc.ca>
2023-08-06 12:17:24 -0700

    click userimage lead to userinfopage

--- frontend/src/components/mainPage/Dropdown.js
+++ frontend/src/components/mainPage/Dropdown.js
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import { useOktaAuth } from '@okta/okta-react';
-import {
-  Menu,
-  Avatar,
-  Box,
-} from '@mui/material';
+import { Menu, Avatar, Box } from '@mui/material';
 import { useUserContext } from '../../auth/UserContext';
 import '../../Styles/Dropdown.css';
 
 const Dropdown = () => {
   const { authState } = useOktaAuth();
-
   const [open, setOpen] = useState(false);
-
   const { userInfo } = useUserContext();
 
   const user_image = userInfo == null ? null : userInfo.userPhotoUrl;
@@ -23,7 +18,9 @@ const Dropdown = () => {
 
   return (
     <Box sx={{ flexGrow: 0 }}>
-      <Avatar alt="Null" src={user_image} />
+      <Link to={`/profile/${userInfo?.userId}`}>
+        <Avatar alt="Null" src={user_image} />
+      </Link>
       <Menu
         sx={{ mt: '45px' }}
         id="menu-appbar"
